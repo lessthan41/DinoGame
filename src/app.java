f819870ea7798f2e1d45cce83710e00e7e93eef2
Wayne Ho <43234148+lessthan41@users.noreply.github.com>
2019-06-04 19:34:03 +0800

    about to OK

--- src/app.java
+++ src/app.java
@@ -1,10 +1,10 @@
 import java.awt.EventQueue;
 
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.KeyStroke;
 import javax.swing.SwingConstants;
+
 import java.awt.Image;
 
 import javax.swing.Action;
@@ -14,6 +14,8 @@ import javax.swing.ImageIcon;
 import javax.swing.InputMap;
 
 import java.awt.Color;
+import java.awt.Cursor;
+
 import javax.swing.JButton;
 import javax.swing.JComponent;
 
@@ -21,18 +23,25 @@ import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.Font;
 
 public class app {
 
 	private JFrame frame;
 	private JLabel dinoLabel;
-	private JButton btnEnd;
+	private JLabel cactusLabel;
+	private JLabel crowLabel;
+	private JLabel gameOver;
+	private JLabel timeCountLabel;
+	private JLabel cloud;
+	private JLabel sun;
 	private JLabel landLabel1;
 	private JLabel landLabel2;
 	private boolean runState = true;
 	private boolean bowState = false;
-	private boolean endState = false;
+	private boolean endState = true;
 	private boolean jumpState = false;
+	private boolean landState = false;
 
 	/**
 	 * Launch the application.
@@ -63,45 +72,67 @@ public class app {
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.WHITE);
-		frame.setTitle("dinodino");
+		frame.setTitle("DinoDino");
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
 		frame.getContentPane().setLayout(null);
+
 
 		/**
 		 * 恐龍圖片
 		 */
 		dinoLabel = new JLabel();
-		
+
 		/* Key Words */
 		KeyStroke Up = KeyStroke.getKeyStroke("UP");
 		KeyStroke Space = KeyStroke.getKeyStroke("SPACE");
 		KeyStroke Down = KeyStroke.getKeyStroke("DOWN");
 		KeyStroke DownRelease = KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0, true);
-		
+
 		/* InputMap & ActionMap */
 		InputMap inputMap = dinoLabel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
 		ActionMap actionMap = dinoLabel.getActionMap();
-		
+
 		/* Jump Event */
 		inputMap.put(Up, "jump");
 		inputMap.put(Space, "jump");
 		actionMap.put("jump", dinoJumpEvent);
-		
+
 		/* Bow Event */
 		inputMap.put(Down, "bow");
 		actionMap.put("bow", dinoBowEvent);
-		
+
 		/* BowRelease Event */
 		inputMap.put(DownRelease, "bowRelease");
-		actionMap.put("bowRelease", dinoBowReleaseEvent);	
-		
-		dinoLabel.setBounds(56, 147, 80, 81);
+		actionMap.put("bowRelease", dinoBowReleaseEvent);
+
+		dinoLabel.setBounds(56, 163, 50, 51);
 		dinoLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		dinoLabel.setIcon(new ImageIcon(
 				new ImageIcon("img//dino1.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
 		frame.getContentPane().add(dinoLabel);
+
+		/**
+		 * 仙人掌
+		 */
+		cactusLabel = new JLabel();
+		cactusLabel.setBounds(450, 183, 18, 34);
+		cactusLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		cactusLabel.setIcon(new ImageIcon(
+				new ImageIcon("img//cactus.png").getImage().getScaledInstance(20, 40, Image.SCALE_DEFAULT)));
+		frame.getContentPane().add(cactusLabel);
 		
+		/**
+		 * 烏鴉
+		 */
+		crowLabel = new JLabel();
+		crowLabel.setBounds(550, 155, 36, 17);
+		crowLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		crowLabel.setIcon(new ImageIcon(
+				new ImageIcon("img//crow1.png").getImage().getScaledInstance(33, 24, Image.SCALE_DEFAULT)));
+		frame.getContentPane().add(crowLabel);
+
 		/**
 		 * 地板圖片1
 		 */
@@ -111,7 +142,7 @@ public class app {
 		landLabel1.setHorizontalAlignment(SwingConstants.CENTER);
 		landLabel1.setIcon(new ImageIcon("img//land.png"));
 		frame.getContentPane().add(landLabel1);
-		
+
 		/**
 		 * 地板圖片2
 		 */
@@ -120,47 +151,162 @@ public class app {
 		landLabel2.setHorizontalAlignment(SwingConstants.CENTER);
 		landLabel2.setIcon(new ImageIcon("img//land.png"));
 		frame.getContentPane().add(landLabel2);
-
+		
+		/**
+		 * Title
+		 */
+		JLabel DinoDino = new JLabel("Dino Dino");
+		DinoDino.setHorizontalAlignment(SwingConstants.CENTER);
+		DinoDino.setFont(new Font("Arial", Font.PLAIN, 20));
+		DinoDino.setBounds(159, 55, 125, 34);
+		frame.getContentPane().add(DinoDino);
+		
+		/**
+		 * Time Label
+		 */
+		timeCountLabel = new JLabel();
+		timeCountLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		timeCountLabel.setFont(new Font("微軟正黑體", Font.PLAIN, 23));
+		timeCountLabel.setBounds(183, 15, 75, 34);
+		frame.getContentPane().add(timeCountLabel);
+		
 		/**
 		 * Start按鈕
 		 */
 		JButton btnStart = new JButton("Start");
+		btnStart.setFont(new Font("Arial", Font.PLAIN, 14));
 		btnStart.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				btnStart.setVisible(false); 
+				DinoDino.setVisible(false);
 				startRun();
 			}
 		});
-		btnStart.setBounds(81, 40, 85, 23);
+		btnStart.setBounds(178, 111, 85, 23);
 		btnStart.getInputMap().put(KeyStroke.getKeyStroke("SPACE"), "none");
 		frame.getContentPane().add(btnStart);
-		
-		
+
 		/**
-		 * End按鈕
+		 * Game Over
 		 */
-		btnEnd = new JButton("End");
-		btnEnd.addMouseListener(new MouseAdapter() {
+		gameOver = new JLabel();
+		gameOver.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				endRun();
+				gameOver.setVisible(false);
+				timeCountLabel.setVisible(true);
+				crowLabel.setBounds(550, 155, 36, 17);
+				startRun();
 			}
 		});
-		btnEnd.setBounds(284, 40, 85, 23);
-		btnEnd.getInputMap().put(KeyStroke.getKeyStroke("SPACE"), "none");
-		frame.getContentPane().add(btnEnd);
-
+		gameOver.setBounds(64, 62, 314, 72);
+		gameOver.setHorizontalAlignment(SwingConstants.CENTER);
+		gameOver.setIcon(new ImageIcon(
+				new ImageIcon("img//gameover.png").getImage().getScaledInstance(180, 70, Image.SCALE_DEFAULT)));
+		gameOver.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		gameOver.setVisible(false); 
+		frame.getContentPane().add(gameOver);
+		
+		/**
+		 * background
+		 */
+		cloud = new JLabel();
+		cloud.setBounds(40, 43, 80, 34);
+		cloud.setIcon(new ImageIcon(
+				new ImageIcon("img//cloud.png").getImage().getScaledInstance(80, 20, Image.SCALE_DEFAULT)));
+		frame.getContentPane().add(cloud);
+		
+		sun = new JLabel();
+		sun.setBounds(350, 25, 60, 60);
+		sun.setIcon(new ImageIcon(
+				new ImageIcon("img//sun.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
+		frame.getContentPane().add(sun);
+		
 	}
 
+/* ------------------------------------------------------------------- */
+	
 	/**
-	 *  恐龍跑 Thread
-	*/
-	private void dinoRunThread(){
+	 * 開跑
+	 */
+	private void startRun() {
+		runState = true;
+		endState = false;
+		bowState = false;
+		jumpState = false;
+
+		addBarrierThread();
+		dinoRunThread();
+		gameStartThread();
+		setTimerThread();
+		detectThread();
+	}
+	
+/* ------------------------------------------------------------------- */
+	
+	/**
+	 * 恐龍跳Event
+	 */
+	Action dinoJumpEvent = new AbstractAction() {
+		/* 表明不同版本間的兼容性 */
+		private static final long serialVersionUID = 1L;
+
+		public void actionPerformed(ActionEvent e) {
+			/* 上一次還沒跳完 & 遊戲還沒開始AARRR */
+			if (jumpState || endState) {
+				return;
+			}
+			jumpState = true;
+			dinoJumpThread();
+		}
+	};
+
+	/**
+	 * 恐龍蹲Event
+	 */
+	Action dinoBowEvent = new AbstractAction() {
+		/* 表明不同版本間的兼容性 */
+		private static final long serialVersionUID = 1L;
+
+		public void actionPerformed(ActionEvent e) {
+			/* 正在跳就不讓你蹲 & 蹲過了還想蹲 & 遊戲根本還沒開始AARRR */
+			if (jumpState || bowState || endState) {
+				return;
+			}
+			runState = false;
+			bowState = true;
+			dinoBowThread();
+		}
+	};
+
+	/**
+	 * 恐龍蹲完Event
+	 */
+	Action dinoBowReleaseEvent = new AbstractAction() {
+		/* 表明不同版本間的兼容性 */
+		private static final long serialVersionUID = 1L;
+
+		public void actionPerformed(ActionEvent e) {
+			if (jumpState || endState) {
+				return;
+			}
+			runState = true;
+			bowState = false;
+			dinoRunThread();
+		}
+	};
+
+/* ------------------------------------------------------------------- */
+	/**
+	 * 恐龍跑 Thread
+	 */
+	private void dinoRunThread() {
 		Thread runThread = new Thread(new Runnable() {
 			@Override
 			public void run() {
 				/* 確保位置正確 */
-				dinoLabel.setBounds(56, 147, 80, 81);
+				dinoLabel.setBounds(56, 163, 50, 51);
 				/* Run */
 				while (runState) {
 					dinoRun();
@@ -169,71 +315,107 @@ public class app {
 				if (endState) {
 					dinoLabel.setIcon(new ImageIcon(new ImageIcon("img//dinocrash.png").getImage().getScaledInstance(50,
 							50, Image.SCALE_DEFAULT)));
+					gameOver.setVisible(true);
 				}
 				/* 脫離跑步狀態就 Return */
-				if(!runState) {
+				if (!runState) {
 					return;
 				}
 
 			}
 
 		});
-		
+
 		runThread.start();
 	}
 
-
 	/**
-	 *  地板跑  Thread
-	*/	
-	private void landRunThread() {
-		Thread landThread = new Thread(new Runnable() {
+	 * 整合GameStart 地板跑&鳥飛行 Thread
+	 */
+	private void gameStartThread() {
+		Thread landRunThread = new Thread(new Runnable() {
 			@Override
 			public void run() {
+				/* Prevent Land Accelerate */
+				if (!landState)
+					landState = true;
+				else
+					return;
+				
+				cloud.setBounds(40, 43, 80, 34);
 				while (!endState) {
 					landRun();
 					/* 補充地板 */
 					if (toolbox.needToAddLand(landLabel1.getLocation().x)) {
-						setLandPosition(landLabel1);
+						addLand(landLabel1);
 					} else if (toolbox.needToAddLand(landLabel2.getLocation().x)) {
-						setLandPosition(landLabel2);
+						addLand(landLabel2);
+					}
+					/* 補充雲 */
+					if(toolbox.needToAddCloud(cloud.getLocation().x)) {
+						addCloud();
 					}
 				}
 				/* Return */
-				if(endState) {
+				if (endState) {
 					return;
 				}
 			}
 
 		});
-		landThread.start();
+		
+		Thread flyThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				/* 確保位置正確 */
+				crowLabel.setBounds(550, 155, 36, 17);
+				/* Run */
+				while (!endState) {
+					crowFly();
+				}
+				/* 脫離跑步狀態就 Return */
+				if (endState) {
+					return;
+				}
+
+			}
+
+		});
+
+		flyThread.start();
+		landRunThread.start();
 	}
-	
-	
+
 	/**
-	 *  恐龍蹲下  Thread
-	*/
+	 * 恐龍蹲下 Thread
+	 */
 	private void dinoBowThread() {
 		Thread bowThread = new Thread(new Runnable() {
 			@Override
 			public void run() {
-				if(!bowState) {
+				if (!bowState || jumpState) {
 					return;
 				}
-				dinoLabel.setBounds(50, 155, 80, 81);
-				while(bowState) {
+				dinoLabel.setBounds(50, 185, 50, 30);
+				while (bowState && !endState) {
 					dinoBow();
 				}
+				if (endState) {
+					dinoLabel.setBounds(56, 163, 50, 51);
+					dinoLabel.setIcon(new ImageIcon(new ImageIcon("img//dinocrash.png").getImage().getScaledInstance(50,
+							50, Image.SCALE_DEFAULT)));
+					gameOver.setVisible(true);
+				}
+				return;
 			}
 
 		});
 		bowThread.start();
 	}
 
-	
 	/**
-	 *  恐龍跳躍  Thread
-	*/
+	 * 恐龍跳躍 Thread
+	 */
 	private void dinoJumpThread() {
 		Thread jumpThread = new Thread(new Runnable() {
 			@Override
@@ -244,89 +426,149 @@ public class app {
 		jumpThread.start();
 	}
 
-	
 	/**
-	 * 開跑
+	 * 加仙人掌&烏鴉 Thread
 	 */
-	private void startRun() {
+	private void addBarrierThread() {
+		Thread cactusThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					/* 防止一大堆仙人掌 */
+					if (landState)
+						return;
+					cactusLabel.setBounds(450, 183, 18, 34);
+					/* 只要還沒結束 */
+					while (!endState) {
+						Thread.sleep(1527);
+						addCactus();
+					}
+				} catch (InterruptedException e) {
+					return;
+				}
+			}
+		});
 		
-		runState = true;
-		endState = false;
+		Thread crowThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					/* 防止一大堆烏鴉*/
+					if (landState)
+						return;
+					cactusLabel.setBounds(450, 183, 18, 34);
+					/* 只要還沒結束 */
+					while (!endState) {
+						Thread.sleep(2260);
+						addCrow();
+					}
+				} catch (InterruptedException e) {
+					return;
+				}
+			}
+		});
 		
-		dinoRunThread();
-		landRunThread();
 
+		Thread monitorThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					while (true) {
+						Thread.sleep(0); // I Don't know why
+						if (endState) {
+							System.out.println("Break(Cactus & crow)");
+							cactusThread.interrupt();
+							crowThread.interrupt();
+							return;
+						}
+					}
+				} catch (InterruptedException e) {
+					return;
+				}
+
+			}
+		});
+
+		cactusThread.start();
+		crowThread.start();
+		monitorThread.start();
 	}
 
-	
 	/**
-	 * 恐龍跳Event
+	 * 偵測撞到Thread
 	 */
-	Action dinoJumpEvent = new AbstractAction() {
-		/* 表明不同版本間的兼容性 */
-		private static final long serialVersionUID = 1L;
-		
-		public void actionPerformed(ActionEvent e) {
-			/* 上一次還沒完成 => Return */
-			if(jumpState) {
+	private void detectThread() {
+
+		Thread detectCactusThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				/* 只要還沒結束 */
+				while (!endState) {
+					if (toolbox.cactusCrashDetect(dinoLabel, cactusLabel)) {
+						endRun();
+					}
+				}
 				return;
 			}
-			jumpState = true;
-	        dinoJumpThread();
-	    }
-	};	
-	
-	
-	/**
-	 * 恐龍蹲Event
-	 */
-	Action dinoBowEvent = new AbstractAction() {
-		/* 表明不同版本間的兼容性 */
-		private static final long serialVersionUID = 1L;
+		});
 		
-		public void actionPerformed(ActionEvent e) {
-			/* 正在跳就不讓你蹲 */
-			if(jumpState) {
+		Thread detectCrowThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				/* 只要還沒結束 */
+				while (!endState) {
+					if (toolbox.crowCrashDetect(dinoLabel, crowLabel)) {
+						endRun();
+					}
+				}
 				return;
 			}
-			runState = false;
-			bowState = true;
-			dinoBowThread();
-	    }
-	};
-	
-	
-	/**
-	 * 恐龍蹲完Event
-	 */
-	Action dinoBowReleaseEvent = new AbstractAction() {
-		/* 表明不同版本間的兼容性 */
-		private static final long serialVersionUID = 1L;
+		});
 		
-		public void actionPerformed(ActionEvent e) {
-			if(jumpState) {
-				return;
+		detectCactusThread.start();
+		detectCrowThread.start();
+	}
+
+	/**
+	 * 分數 Thread
+	 */
+	private void setTimerThread() {
+		
+		Thread timerThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				/* 只要還沒結束 */
+				long start = System.currentTimeMillis();
+				long time;
+		        while (!endState) {
+		            time = System.currentTimeMillis() - start;
+		            float seconds = (float) Math.round((float) time / 10) / 100;
+		            timeCountLabel.setText(seconds + "");
+		            try { Thread.sleep(97); } catch(Exception e) {}
+		        }
+		        if(endState) {
+		        	return;
+		        }
 			}
-			runState = true;
-			bowState = false;
-			dinoRunThread();
-	    }
-	};
+		});
+		
+		timerThread.start();
+	}
 	
-	
-	
+/* ------------------------------------------------------------------- */
+
 	/**
 	 * 恐龍跑步(含計時)
 	 */
 	private void dinoRun() {
 		try {
-			if(!runState) {
+			if (!runState) {
 				return;
 			}
 			dinoLabel.setIcon(new ImageIcon(
 					new ImageIcon("img//dinorun1.png").getImage().getScaledInstance(49, 49, Image.SCALE_DEFAULT)));
 			Thread.sleep(100);
-			if(!runState) {
+			if (!runState) {
 				return;
 			}
 			dinoLabel.setIcon(new ImageIcon(
@@ -336,19 +578,19 @@ public class app {
 			return;
 		}
 	}
-	
+
 	/**
 	 * 恐龍蹲下(含計時)
 	 */
 	private void dinoBow() {
 		try {
-			if(!bowState) {
+			if (!bowState) {
 				return;
 			}
 			dinoLabel.setIcon(new ImageIcon(
 					new ImageIcon("img//dinodown1.png").getImage().getScaledInstance(50, 35, Image.SCALE_DEFAULT)));
 			Thread.sleep(100);
-			if(!bowState) {
+			if (!bowState) {
 				return;
 			}
 			dinoLabel.setIcon(new ImageIcon(
@@ -358,47 +600,115 @@ public class app {
 			return;
 		}
 	}
-	
+
 	/**
 	 * 恐龍跳躍
 	 */
 	private void dinoJump() {
 		try {
-			for (int t = 0; t <= 65; t++) {
-				dinoLabel.setLocation(dinoLabel.getLocation().x, dinoLabel.getLocation().y - 1);
+			for (int t = 0; t <= 40; t++) {
+				dinoLabel.setLocation(dinoLabel.getLocation().x, dinoLabel.getLocation().y - 2);
+				if (endState)
+					return;
 				Thread.sleep(5);
 			}
-			for (int t = 0; t <= 65; t++) {
-				dinoLabel.setLocation(dinoLabel.getLocation().x, dinoLabel.getLocation().y + 1);
+			for (int t = 0; t <= 40; t++) {
+				dinoLabel.setLocation(dinoLabel.getLocation().x, dinoLabel.getLocation().y + 2);
+				if (endState)
+					return;
 				Thread.sleep(5);
 			}
 
 			jumpState = false;
-			
+
 		} catch (InterruptedException e) {
 			return;
 		}
 	}
 
 	/**
-	 * 地板移動(含計時)
+	 * 地板&仙人掌移動&烏鴉移動&雲移動(含計時)
 	 */
 	private void landRun() {
 		try {
-			landLabel1.setLocation(landLabel1.getLocation().x - 2, landLabel1.getLocation().y);
-			landLabel2.setLocation(landLabel2.getLocation().x - 2, landLabel1.getLocation().y);
+			landLabel1.setLocation(landLabel1.getLocation().x - 4, landLabel1.getLocation().y);
+			landLabel2.setLocation(landLabel2.getLocation().x - 4, landLabel1.getLocation().y);
+			cactusLabel.setLocation(cactusLabel.getLocation().x - 4, cactusLabel.getLocation().y);
+			crowLabel.setLocation(crowLabel.getLocation().x - 8, crowLabel.getLocation().y);
+			cloud.setLocation(cloud.getLocation().x - 1, cloud.getLocation().y);
 			Thread.sleep(10);
 		} catch (InterruptedException e) {
 			return;
 		}
 	}
-
+	
 	/**
-	 * 搬地板到最前面(補充地板)
+	 * 烏鴉飛
 	 */
-	private void setLandPosition(JLabel label) {
+	private void crowFly() {
+		try {
+			if (endState) {
+				return;
+			}
+			crowLabel.setIcon(new ImageIcon(
+					new ImageIcon("img//crow1.png").getImage().getScaledInstance(33, 24, Image.SCALE_DEFAULT)));
+			Thread.sleep(100);
+			if (endState) {
+				return;
+			}
+			crowLabel.setIcon(new ImageIcon(
+					new ImageIcon("img//crow2.png").getImage().getScaledInstance(33, 24, Image.SCALE_DEFAULT)));
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			return;
+		}
+	}
+
+/* ------------------------------------------------------------------- */
+	
+	/**
+	 * 加地板(搬地板到最前面補充地板)
+	 */
+	private void addLand(JLabel label) {
 		label.setLocation(436, label.getLocation().y);
 	}
+	
+	/**
+	 * 加仙人掌(拿到最前面)
+	 */
+	private void addCactus() {
+		if (endState) {
+			return;
+		} else if (toolbox.addCactusOrNot()) {
+			System.out.println("ADD CACTUS");
+			cactusLabel.setBounds(450, 183, 18, 34);
+		}
+	}
+
+	/**
+	 * 加烏鴉(拿到最前面)
+	 */
+	private void addCrow() {
+		if (endState) {
+			return;
+		} else if (toolbox.addCrowOrNot()) {
+			System.out.println("ADD CROW");
+			crowLabel.setBounds(550, 155, 36, 17);
+		}
+	}
+	
+	/**
+	 * 加雲(拿到最前面)
+	 */
+	private void addCloud() {
+		if (endState) {
+			return;
+		} else {
+			cloud.setBounds(450, 43, 80, 34);
+		}
+	}
+
+/* ------------------------------------------------------------------- */
 	
 	/**
 	 * 遊戲結束
@@ -406,5 +716,6 @@ public class app {
 	private void endRun() {
 		endState = true;
 		runState = false;
+		landState = false;
 	}
 }
--- src/toolbox.java
+++ src/toolbox.java
@@ -1,3 +1,4 @@
+import javax.swing.JLabel;
 
 public class toolbox {
 
@@ -9,6 +10,90 @@ public class toolbox {
 		boolean ret = (x <= -436) ? true : false;
 		return ret;
 	}
+	
+	public static boolean needToAddCloud(int x) {
+		boolean ret = (x <= -100) ? true : false;
+		return ret;
+	}
+	
+	public static boolean addCactusOrNot() {
+		boolean ret = (Math.random() >= 0.3) ? true : false;
+		return ret;
+	}
+	
+	public static boolean addCrowOrNot() {
+		boolean ret = (Math.random() >= 0.5) ? true : false;
+		return ret;
+	}
+
+	public static boolean cactusCrashDetect(JLabel dinoLabel, JLabel cactusLabel) {
+		try {
+			int whichCase = (dinoLabel.getLocation().x <= (cactusLabel.getLocation().x + cactusLabel.getWidth()/2)) ? 1 : 2;
+			switch(whichCase) {
+				case 1:
+					boolean Xindx = (dinoLabel.getLocation().x + dinoLabel.getWidth()) > cactusLabel.getLocation().x;
+					boolean Yindx = (dinoLabel.getLocation().y + dinoLabel.getHeight()) > cactusLabel.getLocation().y;
+					Thread.sleep(0); // I Don't Know Why
+					
+					if(Xindx && Yindx)
+						return true;
+					else 
+						return false;
+				
+				case 2:
+					boolean Xindx2 = dinoLabel.getLocation().x < (cactusLabel.getLocation().x + cactusLabel.getWidth());
+					boolean Yindx2 = (dinoLabel.getLocation().y + dinoLabel.getHeight()) > cactusLabel.getLocation().y;
+					Thread.sleep(0); // I Don't Know Why
+					
+					if(Xindx2 && Yindx2)
+						return true;
+					else 
+						return false;
+					
+				default:
+					return true;
+			}
+			
+		} catch (InterruptedException e) {
+			return true;
+		}
+
+	}
+	
+	public static boolean crowCrashDetect(JLabel dinoLabel, JLabel crowLabel) {
+		try {
+			int whichCase = (dinoLabel.getLocation().x <= (crowLabel.getLocation().x + crowLabel.getWidth()/2)) ? 1 : 2;
+			switch(whichCase) {
+				case 1:
+					boolean Xindx = (dinoLabel.getLocation().x + dinoLabel.getWidth()) > crowLabel.getLocation().x;
+					boolean Yindx = dinoLabel.getLocation().y < (crowLabel.getLocation().y + crowLabel.getHeight());
+					Thread.sleep(0); // I Don't Know Why
+					
+					if(Xindx && Yindx)
+						return true;
+					else 
+						return false;
+				
+				case 2:
+					boolean Xindx2 = dinoLabel.getLocation().x < (crowLabel.getLocation().x + crowLabel.getWidth());
+					boolean Yindx2 = dinoLabel.getLocation().y < (crowLabel.getLocation().y + crowLabel.getHeight());
+					Thread.sleep(0); // I Don't Know Why
+					
+					if(Xindx2 && Yindx2)
+						return true;
+					else 
+						return false;
+					
+				default:
+					return true;
+			}
+			
+		} catch (InterruptedException e) {
+			return true;
+		}
+
+	}
+
 
       
 }
